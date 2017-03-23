dcbdb5e709bdfa20ceaa0fc1f350a6e9d14b399d
Markus Paeschke <Neoklosch@users.noreply.github.com>
2017-03-23 14:44:26 +0100

    more scripts and a jar

--- docker_container/setup.sh
+++ docker_container/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get update && apt-get -y upgrade && apt-get install -y nano locate man openjdk-7-jdk openjdk-7-demo openjdk-7-doc openjdk-7-jre-headless openjdk-7-jre-lib openjdk-7-source
+apt-get update && apt-get -y upgrade && apt-get install -y nano locate man screen git-core openjdk-7-jdk openjdk-7-demo openjdk-7-doc openjdk-7-jre-headless openjdk-7-jre-lib openjdk-7-source
 apt-get install -y --force-yes apt-transport-https
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 echo "deb https://download.docker.com/linux/ubuntu trusty stable" >> /etc/apt/sources.list
@@ -9,3 +9,16 @@ apt-get install -y docker-ce
 
 echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bashrc
 source ~/.bashrc
+
+# install dummy VNFM
+mkdir /opt/openbaton
+cd /opt/openbaton
+git clone https://github.com/openbaton/dummy-vnfm-amqp.git
+cd /opt/openbaton/dummy-vnfm-amqp
+./dummy-vnfm.sh compile
+
+# install docker plugin
+mkdir /opt/openbaton-docker-plugin
+cd /opt/openbaton-docker-plugin
+git clone https://github.com/Neoklosch/Open-Baton-Docker-Prototype.git
+cp /opt/openbaton-docker-plugin/Open-Baton-Docker-Prototype/jar/open_baton_docker_plugin-1.0-SNAPSHOT.jar /usr/lib/openbaton/plugins/vim-drivers/openbaton-plugin-vimdriver-docker-1.0-SNAPSHOT.jar
---
+++ open-baton-scripts/docker_stuff.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+# run container
+# !!!! Attention: Check the IP !!!!
+docker run -d -h openbaton-rabbitmq -p 8080:8080 -p 5672:5672 -p 15672:15672 -p 8443:8443 -e RABBITMQ_BROKERIP=192.168.178.52 neoklosch/open_baton_docker_test
+
+# build Open Baton project
+./gradlew clean googleJavaFormat build
+
+# copy jar to Open Baton in docker
+docker cp ./build/libs/open_baton_docker_plugin-1.0-SNAPSHOT.jar inspiring_archimedes:/usr/lib/openbaton/plugins/vim-drivers/openbaton-plugin-vimdriver-docker-1.0-SNAPSHOT.jar
+
+# run container and give them a specific name
+docker run -d --name first_container -h openbaton-rabbitmq -p 8080:8080 -p 5672:5672 -p 15672:15672 -p 8443:8443 -e RABBITMQ_BROKERIP=192.168.178.24 1e38eecc6c2b
+
+# enable rabbitmq management
+docker exec 7ad1d7102a98 rabbitmq-plugins enable rabbitmq_management
+
+# build image
+docker build -t neoklosch/open_baton_docker_test .
+
+# commit image
+docker commit bbd42c2ce2a4 neoklosch/open_baton_docker_test
+
+# upload image
+docker push neoklosch/open_baton_docker_test
+
+# start dummy VNFM
+cd /opt/openbaton/dummy-vnfm-amqp && ./dummy-vnfm.sh start
